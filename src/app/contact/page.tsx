02780403bfb45d3c9e4d2daad9866e5db5a8d0fc
EragonRD <debbah.rabah.amir@gmail.com>
2024-06-01 00:54:28 +0200

    form pas finis

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,39 +1,58 @@
+"use client";
 import Image from "next/image";
 import Navbar  from "../components/navbar"
 import Carousel from "../components/caroussel";
 
-export default function Home() {
+
+import React, { useState } from "react";
+
+const ContactForm: React.FC = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Traitez les données du formulaire ici
+    console.log({ name, email, message });
+  };
+
   return (
-
-    <main className="flex min-h-screen flex-col items-center justify-between ">
-      <Navbar />
-      <a>page de contact</a>
-
-      <div className="Form">
-  <form action="/send-data-here" method="post">
-  <label for="first">Nom:</label>
-  <input type="text" id="first" name="first" />
-  <label for="last">Prenom:</label>
-  <input type="text" id="last" name="last" />
-  <div>
-    <button type="submit">envoyer</button>
-  </div>
-  
-  
-</form>
-</div>
-      <div className="logo">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/logo.png"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          
+    
+    <div className="flex flex-col justify-center items-center">
+      <form onSubmit={handleSubmit} className="w-full max-w-md">
+        <label htmlFor="name">Objet:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="border border-gray-400 p-2 mb-4 w-full"
         />
-      </div>
-
-
-    </main>
+        <label htmlFor="email">Adresse e-mail:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="border border-gray-400 p-2 mb-4 w-full"
+        />
+        <label htmlFor="message">Message:</label>
+        <textarea
+          id="message"
+          name="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          className="border border-gray-400 p-2 mb-4 w-full"
+        />
+        <button type="submit" className="bg-blue-500 text-white p-2 w-full">
+          Envoyer le message
+        </button>
+      </form>
+    </div>
   );
-}
+};
+
+export default ContactForm;
